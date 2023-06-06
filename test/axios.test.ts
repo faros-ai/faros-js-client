0a4064987853edc212fa236405cbb5c056a9894a
Ted Tuttle <ted@faros.ai>
2023-06-06 16:26:13 -0700

    FAI-6462: retry http 409 (edit conflict) for graphql endpoint

--- test/axios.test.ts
+++ test/axios.test.ts
@@ -11,6 +11,18 @@ describe('axios', () => {
     transport: {target: 'pino-pretty', options: {levelFirst: true}},
   });
 
+  function mockPostCode(url: string, code: number) {
+    return nock(apiUrl)
+      .post(url)
+      .reply(code)
+      .post(url)
+      .reply(code)
+      .post(url)
+      .reply(code)
+      .post(url)
+      .reply(200, {tenantId: '1'});
+  }
+
   test('get resource with retry', async () => {
     const mock = nock(apiUrl)
       .get('/hi')
@@ -33,16 +45,22 @@ describe('axios', () => {
     mock.done();
   });
 
+  test('get graphql with 409 should fail', async () => {
+    const mock = nock(apiUrl).get('/graphs/foo/graphql').reply(409);
+    const client = sut.makeAxiosInstanceWithRetry(
+      {baseURL: apiUrl},
+      undefined,
+      3,
+      100
+    );
+    await expect(client.get('/graphs/foo/graphql')).rejects.toThrowError(
+      'Request failed with status code 409'
+    );
+    mock.done();
+  });
+
   test('post resource with retry', async () => {
-    const mock = nock(apiUrl)
-      .post('/hi')
-      .reply(502)
-      .post('/hi')
-      .reply(502)
-      .post('/hi')
-      .reply(502)
-      .post('/hi')
-      .reply(200, {tenantId: '1'});
+    const mock = mockPostCode('/hi', 502);
     const client = sut.makeAxiosInstanceWithRetry(
       {baseURL: apiUrl},
       undefined,
@@ -50,6 +68,20 @@ describe('axios', () => {
       100
     );
     const res = await client.post('/hi');
+    expect(res.status).toBe(200);
+    expect(res.data).toStrictEqual({tenantId: '1'});
+    mock.done();
+  });
+
+  test('post graphql with 409 should retry', async () => {
+    const mock = mockPostCode('/graphs/foo/graphql', 409);
+    const client = sut.makeAxiosInstanceWithRetry(
+      {baseURL: apiUrl},
+      undefined,
+      3,
+      100
+    );
+    const res = await client.post('/graphs/foo/graphql');
     expect(res.status).toBe(200);
     expect(res.data).toStrictEqual({tenantId: '1'});
     mock.done();
