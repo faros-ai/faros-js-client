ce8a899df16f0597ccbe3548035ff442b200c643
Chalenge Masekera <chalenge@faros.ai>
2023-07-24 12:28:53 -0400

    suppot delete mutations

--- test/query-builder.test.ts
+++ test/query-builder.test.ts
@@ -3,37 +3,37 @@ import * as sut from '../src/graphql/query-builder';
 const ORIGIN = 'test-origin';
 
 describe('query builder', () => {
+  const qb = new sut.QueryBuilder(ORIGIN);
+
+  const compute_Application = {
+    name: '<application_name>',
+    platform: '<application_platform>',
+  };
+  const cicd_Organization = {
+    uid: '<organization_uid>',
+    source: '<organization_source>',
+  };
+  const cicd_Pipeline = {
+    uid: '<pipeline_uid>',
+    organization: qb.ref({cicd_Organization}),
+  };
+  const cicd_Build = {
+    uid: '<cicd_Build>',
+    pipeline: qb.ref({cicd_Pipeline}),
+    name: '<build_name>',
+  };
+  const cicd_Deployment = {
+    uid: '<deployment_uid',
+    source: '<deployment_source>',
+    application: qb.ref({compute_Application}),
+    build: qb.ref({cicd_Build}),
+    status: {
+      category: 'Success',
+      detail: '<status_detail>',
+    },
+  };
+
   test('creates mutations', () => {
-    const qb = new sut.QueryBuilder(ORIGIN);
-
-    const compute_Application = {
-      name: '<application_name>',
-      platform: '<application_platform>',
-    };
-    const cicd_Organization = {
-      uid: '<organization_uid>',
-      source: '<organization_source>',
-    };
-    const cicd_Pipeline = {
-      uid: '<pipeline_uid>',
-      organization: qb.ref({cicd_Organization}),
-    };
-    const cicd_Build = {
-      uid: '<cicd_Build>',
-      pipeline: qb.ref({cicd_Pipeline}),
-      name: '<build_name>',
-    };
-    const cicd_Deployment = {
-      uid: '<deployment_uid',
-      source: '<deployment_source>',
-      application: qb.ref({compute_Application}),
-      build: qb.ref({cicd_Build}),
-      status: {
-        category: 'Success',
-        detail: '<status_detail>',
-      },
-    };
-
     const mutations = [
       qb.upsert({compute_Application}),
       qb.upsert({cicd_Organization}),
@@ -46,8 +46,6 @@ describe('query builder', () => {
   });
 
   test('creates mutations with non-model objects', () => {
-    const qb = new sut.QueryBuilder(ORIGIN);
-
     const qa_TestCase = {
       uid: '<uid>',
       source: '<source>',
@@ -70,8 +68,6 @@ describe('query builder', () => {
   });
 
   test('creates mutations with undefined and null fields', () => {
-    const qb = new sut.QueryBuilder(ORIGIN);
-
     const qa_TestCase = {
       uid: '<uid>',
       source: '<source>',
@@ -82,6 +78,31 @@ describe('query builder', () => {
     };
 
     const mutations = [qb.upsert({qa_TestCase})];
+    const queryString = sut.batchMutation(mutations);
+    expect(queryString).toMatchSnapshot();
+  });
+
+  test('delete mutations with non-model objects', () => {
+    const qa_TestCase = {
+      uid: '<uid>',
+      source: '<source>',
+      name: '<name>',
+      before: null,
+      after: undefined,
+      tags: ['tag1', 'tag2'],
+    };
+
+    const mutations = [qb.upsert({qa_TestCase})];
+    const queryString = sut.batchMutation(mutations);
+    expect(queryString).toMatchSnapshot();
+  });
+
+
+  test('delete mutations with model refs', () => {
+    const mutations = [
+      qb.delete({cicd_Deployment}),
+      qb.delete({cicd_Build})
+    ];
     const queryString = sut.batchMutation(mutations);
     expect(queryString).toMatchSnapshot();
   });
