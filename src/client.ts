debd4725a258433a33e6ddf7f6a88fd097308566
Chris Wu <cjwu15@gmail.com>
2023-09-13 11:15:47 -0700

    FAI-7137 - Add generic request method to client for calling internal endpoints (#161)

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
-import {AxiosInstance, AxiosRequestConfig} from 'axios';
+import {AxiosInstance, AxiosRequestConfig, Method} from 'axios';
 import * as gql from 'graphql';
 import {get as traverse, isEmpty, unset} from 'lodash';
 import pino, {Logger} from 'pino';
@@ -194,10 +194,10 @@ export class FarosClient {
   queryParameters(): Dictionary<any> {
     const result: Dictionary<any> = {};
     if (this.graphVersion === GraphVersion.V2) {
-        result.phantoms = this.phantoms;
-        if (this.visibility) {
-            result.visibility = this.visibility;
-        }
+      result.phantoms = this.phantoms;
+      if (this.visibility) {
+        result.visibility = this.visibility;
+      }
     }
     return result;
   }
@@ -232,11 +232,10 @@ export class FarosClient {
         headers['content-encoding'] = 'gzip';
         headers['content-type'] = 'application/json';
       }
-      const {data} = await this.api.post(
-        `/graphs/${graph}/graphql`,
-        req,
-        {headers, params}
-      );
+      const {data} = await this.api.post(`/graphs/${graph}/graphql`, req, {
+        headers,
+        params,
+      });
       return data;
     } catch (err: any) {
       throw wrapApiError(err, `unable to query graph: ${graph}`);
@@ -478,6 +477,25 @@ export class FarosClient {
         err,
         `unable to retrieve event: ${eventId} for webhook: ${webhookId}`
       );
+    }
+  }
+
+  async request<T>(
+    method: Method,
+    path: string,
+    data?: any,
+    params?: any
+  ): Promise<T> {
+    try {
+      const response = await this.api.request({
+        method,
+        url: path,
+        data,
+        params,
+      });
+      return response.data;
+    } catch (err: any) {
+      throw wrapApiError(err, `unable to perform request: ${path}`);
     }
   }
 }
