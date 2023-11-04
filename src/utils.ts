65ab8e153f5dbecbff79e9278126b4c661bf1a7f
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2023-11-04 14:56:27 -0400

    Move cleanAndTruncate from to Utils (#169)

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
 import {Duration} from 'luxon';
 import VError from 'verror';
 
+// Max length for free-form description text fields such as issue body
+const MAX_TEXT_LENGTH = 1000;
 
 export class Utils {
   static urlWithoutTrailingSlashes(url: string): string {
@@ -116,7 +118,7 @@ export class Utils {
     });
   }
 
-static toCategoryDetail<EnumType extends {Custom: any}>(
+  static toCategoryDetail<EnumType extends {Custom: any}>(
     enumObject: EnumType & Record<string, any>,
     category: string,
     categoryMapping: Record<string, string> = {}
@@ -137,5 +139,29 @@ static toCategoryDetail<EnumType extends {Custom: any}>(
       category: enumObject.Custom,
       detail: category,
     };
+  }
+
+  static cleanAndTruncate(
+    str?: string,
+    maxLength?: number
+  ): string | null | undefined {
+    if (!str) {
+      return str;
+    }
+    const length = maxLength ?? MAX_TEXT_LENGTH;
+    let result: string;
+    if (str.length <= length) {
+      result = str;
+    } else {
+      // If the last character is part of a unicode surrogate pair,
+      // include the next character
+      const lastChar = str.codePointAt(length - 1) ?? 0;
+      result =
+        lastChar > 65535
+          ? str.substring(0, length + 1)
+          : str.substring(0, length);
+    }
+    // eslint-disable-next-line no-control-regex
+    return result.replace(/\u0000/g, '');
   }
 }
