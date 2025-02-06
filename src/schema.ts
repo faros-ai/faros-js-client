1e77f25cd89ef6c39fbc55b9367e47e99fc90c2d
Ted Tuttle <ted@faros.ai>
2025-02-06 08:33:56 -0800

    FAI-14965: Fixed hotspots in faros destination 1. fix tests

--- src/schema.ts
+++ src/schema.ts
@@ -132,12 +132,12 @@ function toDateAsISOString(
     return new Date(val).toISOString();
   }
   try {
+    // test for integers represented as strings
+    if (/^-?\d+$/.test(val)) {
+      return new Date(toNumber(val)).toISOString();
+    }
     return new Date(val).toISOString();
   } catch {
-    try {
-      return new Date(toNumber(val)).toISOString();
-    } catch {
-      throw new VError('Invalid date: %s', val);
-    }
+    throw new VError('Invalid date: %s', val);
   }
 }
